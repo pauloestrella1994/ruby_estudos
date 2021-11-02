317fcfea6e65c42f5b842b4a4bcb7b59bf2ed92f
pauloestrella <pauloestrella1994@gmail.com>
2021-11-02 18:07:23 -0300

    Add data types patterns

---
+++ basic_concepts/data_types.rb
@@ -0,0 +1,31 @@
+#integer
+int = 10
+puts int.class
+
+#float
+number = 10.01
+puts number.class
+
+#boolean
+boo = true
+boo2 = false
+puts boo.class
+puts boo2.class
+
+#string
+str = "Ola mundo"
+puts str.class
+
+#array
+arr = ["position 0", "position 1"]
+puts arr.class
+puts arr[0]
+
+#symbol
+sym = :ruby_symbol
+puts sym.class
+
+#hash
+hash_table = {'key': 'value'}
+puts hash_table.class
+puts hash_table[:key]
