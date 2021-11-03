37d37744ed7a7729d5feaf00372d6dd62cf01f84
pauloestrella <pauloestrella1994@gmail.com>
2021-11-02 23:42:46 -0300

    Get input and transform to integer

--- basic_concepts/input_print.rb
+++ basic_concepts/input_print.rb
@@ -5,3 +5,10 @@ name = gets.chomp
 
 # '#'{} to concatenate strings
 puts "Hello #{name}!"
+
+print 'Write a number: '
+number_as_string = gets.chomp
+puts "Class: #{number_as_string.class}"
+
+# to_i to pass from input string to integer
+puts "Class: #{number_as_string.to_i.class}"
