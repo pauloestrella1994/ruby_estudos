c092ec40f456c48970c1256913ceece42a15139b
pauloestrella1994 <pauloestrella1994@gmail.com>
2021-11-12 09:44:51 -0300

    Add more lambdas

--- lambda/lambda.rb
+++ lambda/lambda.rb
@@ -23,3 +23,13 @@ end
 
 numbers = [1, 2, 3, 4]
 my_lambda.call(numbers)
+
+def foo(first_lambda, second_lambda)
+    first_lambda.call
+    second_lambda.call
+end
+
+first_lambda = lambda { puts "my first lambda"}
+second_lambda = lambda { puts "my second lambda"}
+
+foo(first_lambda, second_lambda)
